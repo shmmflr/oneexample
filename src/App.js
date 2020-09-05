df3e7ba6f586a6a38f8bdea9c7a33ac24238a661
shmmflr <shmmflr98@gmail.com>
2020-09-06 00:12:13 +0430

    Yes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import { Button } from '@material-ui/core';
+import { Button, Alert, Badge } from 'react-bootstrap';
+import {ToastContainer , Toast, toast} from 'react-toastify'
+
+export class App extends Component {
+  state = {
+    show: true,
+  };
+  show = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+    toast.success('احسنت به این همه شهامت لبیک یا حسین .' ,{
+      position: 'top-left',
+      // autoClose: true,
+      // closeButton :true,
+
+    })
+  };
+  render() {
+    const { show } = this.state;
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <Alert variant="info">
+          <h1>به نام خداوند بخشنده مهربان</h1>
+        </Alert>
+        <Alert variant="secondary">
+          <h5>
+            تهداد حاضران موجود در جلسه{' '}
+            <Badge variant="warning" pill>
+              5
+            </Badge>{' '}
+            می باشد
+          </h5>
+        </Alert>
+        <Button variant={show ? 'info' : 'light'} onClick={this.show} size="">
+          shoeib
+        </Button>
+        <ToastContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
